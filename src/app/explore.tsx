1f6a7d4b2149db82f5ceaa81b01e38a107c917c8
Bryan Ulises <tu_email@example.com>
2026-05-28 16:27:52 -0500

    cardo

--- src/app/explore.tsx
+++ src/app/explore.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
+import * as Speech from "expo-speech";
 
 import QuestionCard from "../components/QuestionCard";
 import ProgressBar from "../components/ProgressBar";
@@ -24,6 +25,33 @@ export default function ExploreScreen() {
   const [history, setHistory] = useState<
     { question: string; answer: string }[]
   >([]);
+
+  // Leer automáticamente cada pregunta
+  useEffect(() => {
+    if (currentNode.question) {
+      Speech.stop();
+
+      Speech.speak(currentNode.question, {
+        language: "es-ES",
+        pitch: 1,
+        rate: 0.9,
+      });
+    }
+
+    // Leer resultado final
+    if (currentNode.career) {
+      Speech.stop();
+
+      Speech.speak(
+        `Tu carrera recomendada es ${currentNode.career}`,
+        {
+          language: "es-ES",
+          pitch: 1,
+          rate: 0.9,
+        }
+      );
+    }
+  }, [currentNode]);
 
   const calculateProgress = (
     current: TreeNode
